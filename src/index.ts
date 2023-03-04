f838c19b8fd8a6f1f1b3042357aae87b0245cf80
AlexStorm <info@alexstorm.ru>
2023-03-04 14:33:18 +0530

    build fixes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export * from './lib/ui-forms.module';
 export * from './lib/fields/text-field/text-field.component'
-export * from '../../controls/src/lib/button/button.component'
 export * from './lib/provider.service'
