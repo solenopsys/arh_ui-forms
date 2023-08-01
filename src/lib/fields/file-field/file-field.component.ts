2867cb3d421f13d84104ad8adcb6b5de84778809
AlexStorm <info@alexstorm.ru>
2023-08-01 20:47:32 +0530

    remove declare

--- src/lib/fields/file-field/file-field.component.ts
+++ src/lib/fields/file-field/file-field.component.ts
@@ -14,7 +14,7 @@ import {ProviderService} from "../../provider.service";
 export class FileFieldComponent
     extends AbstractField<EntityTitle> {
     @Input()
-    declare value: any;
+     value: any;
 
     selectShow = false;
 
